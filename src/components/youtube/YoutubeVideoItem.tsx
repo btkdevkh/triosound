d334e75c27b50142027de2b301ac870b4fa0e64a
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-11-30 22:50:18 +0100

    made some changes

--- src/components/youtube/YoutubeVideoItem.tsx
+++ src/components/youtube/YoutubeVideoItem.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function YoutubeVideoItem({ v }: any) {  
   return (
     <a href={`https://www.youtube.com/watch?v=${v.id.videoId}`} target="_blank">
-      <img src={v.snippet.thumbnails.high.url} alt="thumbnails" />
-      <span>{v.snippet.title.slice(0, 50)}...</span>
+      <img src={v.snippet.thumbnails.medium.url} alt="thumbnails" />
+      <span>{v.snippet.title.slice(0, 40)}...</span>
     </a>
   )
 }
